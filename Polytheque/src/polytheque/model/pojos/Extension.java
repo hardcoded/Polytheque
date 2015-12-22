e7b1f42648aefdf4a355b17b161a528ffbe84f65
JohanBrunet <brunet.johan.23@gmail.com>
2015-12-22 13:00:15 +0100

    Création du POJO Extension

---
+++ Polytheque/src/polytheque/model/pojos/Extension.java
@@ -0,0 +1,73 @@
+package polytheque.model.pojos;
+
+/**
+ * Classe permettant de représenter une extension de jeu
+ * 
+ * @author Johan Brunet
+ *
+ */
+public class Extention {
+
+	private int idExtension;
+	private String nom;
+	private String description;
+	private String statut;
+	private int nbExemplaires;
+	private int nbReserves;
+
+	public Extention(String nom, String desc, String statut, int nbExemplaires, int nbReserves) {
+		this.setNom(nom);
+		this.setDescription(desc);
+		this.setStatut(statut);
+		this.setNbExemplaires(nbExemplaires);
+		this.setNbReserves(nbReserves);
+	}
+
+	public int getIdExtension() {
+		return idExtension;
+	}
+
+	public void setIdExtension(int idExtension) {
+		this.idExtension = idExtension;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStatut() {
+		return statut;
+	}
+
+	public void setStatut(String statut) {
+		this.statut = statut;
+	}
+
+	public int getNbExemplaires() {
+		return nbExemplaires;
+	}
+
+	public void setNbExemplaires(int nbExemplaires) {
+		this.nbExemplaires = nbExemplaires;
+	}
+
+	public int getNbReserves() {
+		return nbReserves;
+	}
+
+	public void setNbReserves(int nbReserves) {
+		this.nbReserves = nbReserves;
+	}
+}
