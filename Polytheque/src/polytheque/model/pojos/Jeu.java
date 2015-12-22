14a975f22336f1b6be04f7906098dce59900d8c0
JohanBrunet <brunet.johan.23@gmail.com>
2015-12-22 12:53:07 +0100

    Ajout idJeu à la classe Jeu

--- Polytheque/src/polytheque/model/pojos/Jeu.java
+++ Polytheque/src/polytheque/model/pojos/Jeu.java
@@ -2,19 +2,20 @@ package polytheque.model.pojos;
 
 public class Jeu {
 
-   private String nom;
-   private String description;
-   private int ageMini;
-   private String edition;
-   private boolean disponibilite;
-   private String etat;
-   private String status;
-   private String anneeParution;
-   private int nbExemplaires;
-   private int nbReserves;
-   private int nbJoueurs;
-   
-   public Jeu(String nom, String description, int ageMini, String edition, boolean disponibilite, String etat,
+	private int idJeu;
+	private String nom;
+	private String description;
+	private int ageMini;
+	private String edition;
+	private boolean disponibilite;
+	private String etat;
+	private String status;
+	private String anneeParution;
+	private int nbExemplaires;
+	private int nbReserves;
+	private int nbJoueurs;
+
+	public Jeu(String nom, String description, int ageMini, String edition, boolean disponibilite, String etat,
 			String statut, String anneeParution, int nbExemplaires, int nbReserves, int nbJoueurs) {
 		this.setNom(nom);
 		this.setDescription(description);
@@ -29,91 +30,99 @@ public class Jeu {
 		this.setNbJoueurs(nbJoueurs);
 	}
 
-   public String getNom() {
-	   return nom;
-   }
-   
-   public void setNom(String nom) {
-	   this.nom = nom;
-   }
-   
-   public String getDescription() {
-	   return description;
-   }
-   
-   public void setDescription(String description) {
-	   this.description = description;
-   }
-   
-   public int getAgeMini() {
-	   return ageMini;
-   }
-   
-   public void setAgeMini(int ageMini) {
-	   this.ageMini = ageMini;
-   }
-   
-   public String getEdition() {
-	   return edition;
-   }
-   
-   public void setEdition(String edition) {
-	   this.edition = edition;
-   }
-   
-   public boolean isDisponibilite() {
-	   return disponibilite;
-   }
-   
-   public void setDisponibilite(boolean disponibilite) {
-	   this.disponibilite = disponibilite;
-   }
-   
-   public String getEtat() {
-	   return etat;
-   }
-   
-   public void setEtat(String etat) {
-	   this.etat = etat;
-   }
-   
-   public String getStatus() {
-	   return status;
-   }
-   
-   public void setStatus(String status) {
-	   this.status = status;
-   }
-   
-   public String getAnneeParution() {
-	   return anneeParution;
-   }
-   
-   public void setAnneeParution(String anneeParution) {
-	   this.anneeParution = anneeParution;
-   }
+	public String getNom() {
+		return nom;
+	}
 
-   public int getNbExemplaires() {
-	   return nbExemplaires;
-   }
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 
-   public void setNbExemplaires(int nbExemplaires) {
-	   this.nbExemplaires = nbExemplaires;
-   }
+	public String getDescription() {
+		return description;
+	}
 
-   public int getNbReserves() {
-	   return nbReserves;
-   }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-   public void setNbReserves(int nbReserves) {
-	   this.nbReserves = nbReserves;
-   }
+	public int getAgeMini() {
+		return ageMini;
+	}
 
-   public int getNbJoueurs() {
-	   return nbJoueurs;
-   }
+	public void setAgeMini(int ageMini) {
+		this.ageMini = ageMini;
+	}
 
-   public void setNbJoueurs(int nbJoueurs) {
-	   this.nbJoueurs = nbJoueurs;
-   }
+	public String getEdition() {
+		return edition;
+	}
+
+	public void setEdition(String edition) {
+		this.edition = edition;
+	}
+
+	public boolean isDisponibilite() {
+		return disponibilite;
+	}
+
+	public void setDisponibilite(boolean disponibilite) {
+		this.disponibilite = disponibilite;
+	}
+
+	public String getEtat() {
+		return etat;
+	}
+
+	public void setEtat(String etat) {
+		this.etat = etat;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getAnneeParution() {
+		return anneeParution;
+	}
+
+	public void setAnneeParution(String anneeParution) {
+		this.anneeParution = anneeParution;
+	}
+
+	public int getNbExemplaires() {
+		return nbExemplaires;
+	}
+
+	public void setNbExemplaires(int nbExemplaires) {
+		this.nbExemplaires = nbExemplaires;
+	}
+
+	public int getNbReserves() {
+		return nbReserves;
+	}
+
+	public void setNbReserves(int nbReserves) {
+		this.nbReserves = nbReserves;
+	}
+
+	public int getNbJoueurs() {
+		return nbJoueurs;
+	}
+
+	public void setNbJoueurs(int nbJoueurs) {
+		this.nbJoueurs = nbJoueurs;
+	}
+
+	public int getIdJeu() {
+		return idJeu;
+	}
+
+	public void setIdJeu(int idJeu) {
+		this.idJeu = idJeu;
+	}
 }
