2495b83f19e2bc850b673177d21b0a3ac5362ddc
JohanBrunet <brunet.johan.23@gmail.com>
2015-12-30 15:54:00 +0100

    Ajout de boutons (reste des problèmes de mise en forme)
    Problème pour l'affichage des noms de colonnes du tableau (ils ne
    s'affichent pas).
    Problème pour le positionnement des boutons.

--- Polytheque/src/polytheque/view/AffichageListeJeux.java
+++ Polytheque/src/polytheque/view/AffichageListeJeux.java
@@ -1,9 +1,13 @@
 package polytheque.view;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 import polytheque.view.modeles.ModeleTableauListeJeux;
@@ -11,31 +15,107 @@ import polytheque.view.modeles.ModeleTableauListeJeux;
 @SuppressWarnings("serial")
 public class AffichageListeJeux extends JPanel implements ActionListener {
 
-	private TacheDAffichage afficheAppli;
+
+	public final static int LONGUEUR_COLONNE_0 = 100;
+	public final static int LONGUEUR_COLONNE_1 = 200;
+	public final static int LONGUEUR_COLONNE_2 = 50;
+	public final static int LONGUEUR_COLONNE_3 = 50;
+	public final static int LONGUEUR_COLONNE_4 = 50;
+	public final static int LONGUEUR_COLONNE_5 = 50;
+	public final static int LONGUEUR_COLONNE_6 = 50;
+	public final static int LONGUEUR_COLONNE_7 = 50;
+	
+	/**
+	 * Hauteur des lignes.
+	 */
+	public final static int HAUTEUR_DES_LIGNES = 35;
 	
 	/**
 	 * Les libellés des entêtes.
 	 */
 	public final static String[] LIBELLES = new String[] {"Nom", "Descritpion", "Année de parution", "Statut", "Age mini", "Nombre de joueurs mini", "Catégorie", "Editeur"};
 	
+	/**
+	 * Un bouton.
+	 */
+	private JButton boutonAjouterJeu;
+	
+	/**
+	 * Un bouton.
+	 */
+	private JButton boutonModifierJeu;
+
+	/**
+	 * Un bouton.
+	 */
+	private JButton boutonSupprimerJeu;
+	
+	/**
+	 * Une tache d'affichage de l'application.
+	 */
+	private TacheDAffichage tacheDAffichageDeLApplication;
 	
 	public AffichageListeJeux(TacheDAffichage afficheAppli) {
-		this.afficheAppli = afficheAppli;
+		this.tacheDAffichageDeLApplication = afficheAppli;
 		
-		creerContenu();
+		creerTableau();
+		ajouterLesBoutons();
 	}
 	
-	public void creerContenu() {
+	public void creerTableau() {
 		
         Object[][] donnees = {
-                {"Nom1", "TEST", "2015", "OK", "3", "2", "Aventure", ""},
-                {"Nom2", "test", "1995", "Abimé", "8", "2", "Action", "Moi"},
-                {"Nom3", "Description", "2001", "OK", "12", "4", "", "Lui"},
-                {"Nom4", "Jeu de société", "2012", "OK", "3", "6", "Société", "Toi"},
+                {"Nom1", "TEST", "2015", "OK", "3 ans", "2 pers.", "Aventure", ""},
+                {"Nom2", "test", "1995", "Abimé", "8 ans", "2 pers.", "Action", "Moi"},
+                {"Nom3", "Description", "2001", "OK", "12 ans", "4 pers.", "", "Lui"},
+                {"Nom4", "Jeu de société", "2012", "OK", "3 ans", "6 pers.", "Société", "Toi"},
         };
- 
+		
         JTable tableau = new JTable(new ModeleTableauListeJeux(donnees, LIBELLES));
-        this.add(tableau);
+		
+        tableau.getColumn(LIBELLES[0]).setPreferredWidth(LONGUEUR_COLONNE_0);
+		tableau.getColumn(LIBELLES[1]).setPreferredWidth(LONGUEUR_COLONNE_1);
+		tableau.getColumn(LIBELLES[2]).setPreferredWidth(LONGUEUR_COLONNE_2);
+		tableau.getColumn(LIBELLES[3]).setPreferredWidth(LONGUEUR_COLONNE_3);
+		tableau.getColumn(LIBELLES[4]).setPreferredWidth(LONGUEUR_COLONNE_4);
+		tableau.getColumn(LIBELLES[5]).setPreferredWidth(LONGUEUR_COLONNE_5);
+		tableau.getColumn(LIBELLES[6]).setPreferredWidth(LONGUEUR_COLONNE_6);
+		tableau.getColumn(LIBELLES[7]).setPreferredWidth(LONGUEUR_COLONNE_7);
+		
+		tableau.setRowHeight(HAUTEUR_DES_LIGNES);
+		
+		tableau.getTableHeader().setReorderingAllowed(false);
+		tableau.getTableHeader().setResizingAllowed(false);
+		
+		this.add(new JScrollPane(tableau), BorderLayout.CENTER);
+		
+		this.add(tableau);
+	}
+	
+	/**
+	 * Ajouter les boutons dans la this.
+	 */
+	private void ajouterLesBoutons() {
+		
+		this.boutonAjouterJeu = new JButton("Ajouter un jeu");
+		this.boutonAjouterJeu.setBounds(800, 200, 400, 30);
+		this.boutonAjouterJeu.setFocusable(false);
+		this.boutonAjouterJeu.addActionListener(this);
+		this.add(this.boutonAjouterJeu);
+		
+		this.boutonModifierJeu = new JButton("Modifier un jeu");
+		this.boutonModifierJeu.setBounds(800, 400, 400, 30);
+		this.boutonModifierJeu.setFocusable(false);
+		this.boutonModifierJeu.addActionListener(this);
+		this.add(this.boutonModifierJeu);
+		
+		this.boutonSupprimerJeu = new JButton("Supprimer un jeu");
+		this.boutonSupprimerJeu.setBounds(800, 600, 400, 30);
+		this.boutonSupprimerJeu.setFocusable(false);
+		this.boutonSupprimerJeu.addActionListener(this);
+		this.add(this.boutonSupprimerJeu);
+		
+		this.updateUI();
 	}
 	
 	@Override
