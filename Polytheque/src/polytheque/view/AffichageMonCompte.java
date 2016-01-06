c947b931818fe7eeb1fc89daa71aaeed9d1900b9
GodefroiRoussel <gofff110@gmail.com>
2016-01-06 13:07:18 +0100

    Toutes les données peuvent etre modif et afficher

--- Polytheque/src/polytheque/view/AffichageMonCompte.java
+++ Polytheque/src/polytheque/view/AffichageMonCompte.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -69,7 +70,6 @@ public class AffichageMonCompte extends JPanel implements ActionListener {
 			this.userName = new JLabel(this.adherentCourant.getNom());
 			this.userName.setBounds(350, 150, 100, 30);
 			this.add(this.userName);
-			//this.adherentDAO.retreive(this.adherentCourant.getNom())
 			
 			JLabel labelUserFirstName = new JLabel("Prenom :");
 			labelUserFirstName.setBounds(300, 180, 100, 30);
@@ -77,79 +77,71 @@ public class AffichageMonCompte extends JPanel implements ActionListener {
 			this.userFirstName = new JLabel(this.adherentCourant.getPrenom());
 			this.userFirstName.setBounds(360, 180, 100, 30);
 			this.add(this.userFirstName);
-			//this.adherentDAO.retreive(this.adherentCourant.getPrenom())
 			
 			
 			JLabel labelUserBirthday = new JLabel("Date de naissance :");
 			labelUserBirthday.setBounds(300, 210, 150, 30);
 			this.add(labelUserBirthday);
-			this.userBirthday = new JLabel("TEST");
+			this.userBirthday = new JLabel(this.adherentCourant.getDateNaissance().toString());
 			this.userBirthday.setBounds(420, 210, 100, 30);
 			this.add(this.userBirthday);
-			//this.adherentDAO.retreive(this.adherentCourant.getDateNaissance())
 			
 			
 			JLabel labelUserRue = new JLabel("Rue :");
 			labelUserRue.setBounds(300, 240, 100, 30);
 			this.add(labelUserRue);
-			this.userRue = new JTextField("TEST");
+			this.userRue = new JTextField(this.adherentCourant.getRue());
 			this.userRue.setBounds(350, 240, 100, 30);
 			this.add(this.userRue);
-			//this.adherentDAO.retreive(this.adherentCourant.getRue())
 			
 			
 			
 			JLabel labelUserCP = new JLabel("Code Postal:");
 			labelUserCP.setBounds(300, 270, 100, 30);
 			this.add(labelUserCP);
-			this.userCP = new JTextField("TEST");
+			this.userCP = new JTextField(this.adherentCourant.getCP());
 			this.userCP.setBounds(390, 270, 100, 30);
 			this.add(this.userCP);
-			//this.adherentDAO.retreive(this.adherentCourant.getCP())
+	
 			
 			JLabel labelUserVille = new JLabel("Ville :");
 			labelUserVille.setBounds(300, 300, 100, 30);
 			this.add(labelUserVille);
-			this.userVille = new JTextField("TEST");
+			this.userVille = new JTextField(this.adherentCourant.getVille());
 			this.userVille.setBounds(350, 300, 100, 30);
 			this.add(this.userVille);
-			//this.adherentDAO.retreive(this.adherentCourant.getVille())
 			
 			JLabel labelUserMail = new JLabel("Mail :");
 			labelUserMail.setBounds(300, 330, 100, 30);
 			this.add(labelUserMail);
-			this.userMail = new JTextField("TEST");
+			this.userMail = new JTextField(this.adherentCourant.getMail());
 			this.userMail.setBounds(350, 330, 100, 30);
 			this.add(this.userMail);
-			//this.adherentDAO.retreive(this.adherentCourant.getMail())
 			
 			
 			JLabel labelUserTelephone = new JLabel("Telephone :");
 			labelUserTelephone.setBounds(300, 360, 100, 30);
 			this.add(labelUserTelephone);
-			this.userPhone = new JTextField("TEST");
+			this.userPhone = new JTextField(this.adherentCourant.getTelephone());
 			this.userPhone.setBounds(370, 360, 100, 30);
 			this.add(this.userPhone);
-			//this.adherentDAO.retreive(this.adherentCourant.getTelephone())
 			
 			
 			JLabel labelUserPseudo = new JLabel("Pseudo :");
 			labelUserPseudo.setBounds(300, 390, 100, 30);
 			this.add(labelUserPseudo);
-			this.userPseudo = new JLabel("TEST");
+			this.userPseudo = new JLabel(this.adherentCourant.getPseudo());
 			this.userPseudo.setBounds(360, 390, 100, 30);
 			this.add(this.userPseudo);
-			//this.adherentDAO.retreive(this.adherentCourant.getPseudo())
 			
 
 			JLabel labelpassword = new JLabel("Mot de passe :");
 			labelpassword.setBounds(300, 420, 100, 30);
 			this.add(labelpassword);
-			this.password = new JPasswordField("TEST");
+			this.password = new JPasswordField(this.adherentCourant.getMdp());
 			this.password.setBounds(380, 420, 190, 30);
 			this.password.setColumns(10);
 			this.add(this.password);
-			//this.adherentDAO.retreive(this.adherentCourant.getMdp())
 		}
 		
 		public void ajouterBoutons(){
