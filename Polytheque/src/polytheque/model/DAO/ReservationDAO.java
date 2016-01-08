18f6a795a51bf52883a4f6d7235fecf2c1ec6839
LaureMarchal <marchal.laure91@gmail.com>
2016-01-08 19:04:11 +0100

    ensore un essai de la requete

--- Polytheque/src/polytheque/model/DAO/ReservationDAO.java
+++ Polytheque/src/polytheque/model/DAO/ReservationDAO.java
@@ -119,11 +119,10 @@ public class ReservationDAO extends DAO {
 		ArrayList<Reservation> toutesLesReservations = new ArrayList<>();
 		try {
 			super.connect(); //requete fausse à voir
-			PreparedStatement psSelect = connection.prepareStatement("SELECT ADHERENT.nom as nom_adh,ADHENRENT.prenom as prenom_adh,JEU.nom as titre,RESERVATION.date_reservation"
+			PreparedStatement psSelect = connection.prepareStatement("SELECT ADHERENT.nom,ADHENRENT.prenom,JEU.nom,RESERVATION.date_reservation"
 					+ "FROM RESERVATION "
 					+ "JOIN ADHERENT ON ADHERENT.id_adherent = RESERVATION.id_adherent"
-					+ "JOIN JEU ON JEU.id_jeu = RESERVATION.id_jeu"
-					+ "ORDER BY nom_adh ASC");
+					+ "JOIN JEU ON JEU.id_jeu = RESERVATION.id_jeu");
 			psSelect.execute();
 			psSelect.closeOnCompletion();
 
