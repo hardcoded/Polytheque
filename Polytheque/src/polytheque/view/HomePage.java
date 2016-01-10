074a0a348551dc83c1884173bc504119247c827d
LaureMarchal <marchal.laure91@gmail.com>
2016-01-10 17:19:35 +0100

    test

--- Polytheque/src/polytheque/view/HomePage.java
+++ Polytheque/src/polytheque/view/HomePage.java
@@ -33,7 +33,7 @@ public class HomePage extends JTabbedPane {
 		if (this.tacheDAffichageDeLApplication.adherentAdmin()) {
 			this.addTab("Emprunt", new JLabel("Fonctionnalité à venir !"));
 			this.addTab("Adhérents", this.tacheDAffichageDeLApplication.afficherGestionAdherent());
-			//this.addTab("Reservations", this.tacheDAffichageDeLApplication.afficherListeReservations());
+			this.addTab("Reservations", this.tacheDAffichageDeLApplication.afficherListeReservations());
 		}
 	}
 }
