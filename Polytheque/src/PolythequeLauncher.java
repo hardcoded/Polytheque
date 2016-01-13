a573bdc7f8632d53016c665a54620262f0279781
LaureMarchal <marchal.laure91@gmail.com>
2016-01-13 19:09:53 +0100

    commentaires

--- Polytheque/src/PolythequeLauncher.java
+++ Polytheque/src/PolythequeLauncher.java
@@ -1,6 +1,11 @@
 import polytheque.controller.PolythequeApplication;
 import polytheque.view.TacheDAffichage;
 
+/**
+ * Classe permettant l'éxecution du programme
+ * @author Johan Brunet
+ *
+ */
 public class PolythequeLauncher {
 
 	public static void main(String[] args) {
